2c7408f72be0418043f76c8832af414c53f4c0c3
romana_erdelji <romana.erdelji.praksa@intens.rs>
2021-05-14 17:29:04 +0200

    spring boot + angular initialization

---
+++ recommendation-and-troubleshooting/src/main/java/project/recommendationandtroubleshooting/RecommendationAndTroubleshootingApplication.java
@@ -0,0 +1,13 @@
+package project.recommendationandtroubleshooting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationAndTroubleshootingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationAndTroubleshootingApplication.class, args);
+	}
+
+}
---
+++ recommendation-and-troubleshooting/src/test/java/project/recommendationandtroubleshooting/RecommendationAndTroubleshootingApplicationTests.java
@@ -0,0 +1,13 @@
+package project.recommendationandtroubleshooting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationAndTroubleshootingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
