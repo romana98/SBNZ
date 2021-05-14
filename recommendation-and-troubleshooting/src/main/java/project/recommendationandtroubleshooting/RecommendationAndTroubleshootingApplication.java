057debd0087cb50a209ecb14abd35ea12ee43609
romana98 <romana_1998@hotmail.com>
2021-05-14 17:53:16 +0200

    addded dependecies

--- recommendation-and-troubleshooting/src/main/java/project/recommendationandtroubleshooting/RecommendationAndTroubleshootingApplication.java
+++ recommendation-and-troubleshooting/src/main/java/project/recommendationandtroubleshooting/RecommendationAndTroubleshootingApplication.java
@@ -1,13 +1,21 @@
 package project.recommendationandtroubleshooting;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecommendationAndTroubleshootingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecommendationAndTroubleshootingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationAndTroubleshootingApplication.class, args);
+    }
+
+    @Bean
+    public KieContainer kieContainer() {
+        return KieServices.Factory.get().getKieClasspathContainer();
+    }
 
 }
