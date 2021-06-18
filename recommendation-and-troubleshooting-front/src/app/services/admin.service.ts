b6c6362d8fe4507b429e9ac04dc8611253780fc6
romana98 <romana_1998@hotmail.com>
2021-06-18 19:34:16 +0200

    navigation fix

--- recommendation-and-troubleshooting-front/src/app/services/admin.service.ts
+++ recommendation-and-troubleshooting-front/src/app/services/admin.service.ts
@@ -6,16 +6,16 @@ import {UserModel} from "../model/user-model";
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class AdminService {
 
   constructor(private http: HttpClient) { }
 
   getProfile(id: String): Observable<UserModel> {
-    return this.http.get<UserModel>('http://localhost:8080/users/' + id)
+    return this.http.get<UserModel>('http://localhost:8080/administrators/' + id)
   }
 
   editProfile(user: UserModel): Observable<UserModel> {
-    return this.http.put<UserModel>('http://localhost:8080/users',
+    return this.http.put<UserModel>('http://localhost:8080/administrators',
       user);
   }
 }
